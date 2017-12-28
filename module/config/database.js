7e8aa6ef734a76bff6a2c6724da634b791d76426
hansonfang <1597700958@qq.com>
2017-12-28 20:14:54 +0800

    fix mysql lose connection error

--- module/config/database.js
+++ module/config/database.js
@@ -1,9 +1,35 @@
-module.exports = {
+const mysql=require("mysql");
+let config={
     'connection': {
-        'host': 'xxx.xxx.xxx.xxx',
+        'host': 'xxx,xxx,xxx,xxx',
         'user': 'root',
-        'password': '********'
+        'password': '******'
     },
-    'database': 'select one',
+    'database': 'cloudprint',
     'users_table': 'users'
 };
+
+let connection;
+function handleDisconnect() {
+    connection = mysql.createConnection(config.connection); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+
+    connection.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 3000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+module.exports =config;
